2089efdd9ba66e99d58134110b0bdb44ea5d3d6a
Lim <chris@pocketlim.com>
2026-02-28 14:31:24 -0800

    Adding usability wins: - When running the app again, if there is already an existing instance, find that window and bring it to the front - If the app is running in the tray, left clicking the tray icon should open it.

--- NvidiaDisplayController/Interface/Shell/ShellView.xaml.cs
+++ NvidiaDisplayController/Interface/Shell/ShellView.xaml.cs
@@ -10,6 +10,8 @@ using NvidiaDisplayController.Global;
 using NvidiaDisplayController.Global.Controllers;
 using NvidiaDisplayController.Global.Extensions;
 using Application = System.Windows.Application;
+using System.Windows.Interop;
+using System.Windows.Input;
 
 namespace NvidiaDisplayController.Interface.Shell;
 
@@ -25,6 +27,15 @@ public partial class ShellView
 
     [Inject] public DataController DataController { get; set; } = null!;
 
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        // add message handler to listen for messages from other instances of the app
+        HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+        source.AddHook(WndProc);
+    }
+
     private void Start()
     {
         IoC.BuildUp(this);
@@ -32,6 +43,17 @@ public partial class ShellView
         CreateSystemTrayIcon();
 
         GlobalEvents.UpdateToolTip += OnUpdateToolTip;
+    }
+
+    private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        // listen for the custom message and show the window contents
+        if (msg == 0x0400 + 1) // WM_SHOWME
+        {
+            DoShow();
+            handled = true;
+        }
+        return IntPtr.Zero;
     }
 
     private void OnUpdateToolTip()
@@ -44,6 +66,11 @@ public partial class ShellView
         _notifyIcon = new NotifyIcon();
         _notifyIcon.Icon = new Icon("Resources/desktop.ico");
         _notifyIcon.Visible = true;
+
+        // show the window when left clicking the tray icon
+        _notifyIcon.MouseClick += (s, e) => { 
+            if (e.Button == MouseButtons.Left) DoShow(); 
+        };
 
         _notifyIcon.ContextMenuStrip = new ContextMenuStrip();
         _notifyIcon.ContextMenuStrip.Items.Add("Show", null, OpenEvent);
@@ -80,10 +107,19 @@ public partial class ShellView
         DoShow();
     }
 
-    private void DoShow()
+    public void DoShow()
     {
         Show();
         WindowState = WindowState.Normal;
+
+        // ensure to focus the window so that it brings it to the front
+        Activate();
+        Focus();
+
+        // toggle topmost to bring it to the front above all other windows
+        // then disable so it behaves normally
+        Topmost = true;
+        Topmost = false;
     }
 
     protected override void OnStateChanged(EventArgs e)
